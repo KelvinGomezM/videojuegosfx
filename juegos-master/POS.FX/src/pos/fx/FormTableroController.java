67e693643203ba23da8ba09fb159e39050b35f5f
KelvinGomezM <kelvin.gomezm@gmail.com>
2020-05-11 06:24:09 -0600

    master

--- juegos-master/POS.FX/src/pos/fx/FormTableroController.java
+++ juegos-master/POS.FX/src/pos/fx/FormTableroController.java
@@ -1,12 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-<<<<<<< HEAD
 
-=======
->>>>>>> 5792fa80f188baa432ddf06183e879a1db82f69c
 package pos.fx;
 
 import java.net.URL;
@@ -76,11 +68,9 @@ public class FormTableroController implements Initializable {
         facturasServicio = new FacturasServicio();
         
         cargarDatosdelBarChart();
-<<<<<<< HEAD
         cargarDatosdelPieChart();
-=======
+
         //cargarDatosdelPieChart();
->>>>>>> 5792fa80f188baa432ddf06183e879a1db82f69c
         cargarDatosUltimasFacturasEmitidas();
     }    
 
